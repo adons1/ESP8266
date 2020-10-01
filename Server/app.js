161d5c9233105381a253c6480cdc16b0436de004
adons1 <32033752+adons1@users.noreply.github.com>
2020-10-01 21:07:12 +0300

    Add files via upload

--- Server/app.js
+++ Server/app.js
@@ -2,9 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+global.telemetry = "";
 // создаем парсер для данных application/x-www-form-urlencoded
 const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+app.get("/telemetry.html", function (request, response) {
+     response.sendFile(__dirname + "/telemetry.html");
+});
+
+app.get("/get_telemetry", function (request, response) {
+     response.send(global.telemetry);
+});
 
 app.get("/index.html", function (request, response) {
      response.sendFile(__dirname + "/index.html");
@@ -19,6 +27,7 @@ app.post("/index", urlencodedParser, function (request, response) {
 app.post("/data.json", urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body.telemetry);
+    global.telemetry=request.body.telemetry;
     response.send("Successful");
 });
 
